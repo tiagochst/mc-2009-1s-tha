5efb34b08b91aa963da52513b1dd20a4ac4a7667
tiagochst@gmail.com <tiagochst@gmail.com@ab741a24-0e9a-11de-99c7-9d618077940f>
2009-03-16 22:04:59 +0000

    encontra valor na linha a atualizar,ainda não atualiza

--- trunk/Animais silvestres - monitoramento/main.c
+++ trunk/Animais silvestres - monitoramento/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 #include "defines.h"
 #include "funcoes.h"
@@ -10,7 +11,9 @@ int main()
 {
   FILE *fpe;
   char opcao;
-
+  char pos[50],linha[50],*result; 
+  char procura[50];
+  char deli;
   printf(OPT_IND);
   scanf("%c",&opcao);
 
@@ -40,6 +43,37 @@ int main()
     fclose(fpe);
     break;
   
+  case 'A':
+   
+    fpe=fopen("individuos.txt","r+");
+    printf("o que voce procura: \n");  
+    scanf("%s",procura);
+    while(fgetc(fpe)!=EOF){
+      fgets(linha,50,fpe);  
+      result = strtok( linha, "#" );
+      
+      while( result != NULL ) {
+	if(strcmp(result,procura)==0){
+	  printf("encontrado %s\n",result);
+	}  
+	result = strtok( NULL,"#" );
+	
+      }
+    }
+    fclose(fpe);
+      
+ break;
+    
+  case 'R':
+    break;
+
+  case 'V':
+    break;
+
+  case 'S':
+    exit(1);
+    break;
+
   }
   return 0;
 }
