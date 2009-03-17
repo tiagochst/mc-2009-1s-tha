ba09f2f85a3acff1bcd2a0815b1c796fbaa1a2e1
tiagochst <tiagochst@ab741a24-0e9a-11de-99c7-9d618077940f>
2009-03-17 18:21:44 +0000

    atulizar implemenando

--- Animais silvestres - monitoramento/main.c
+++ Animais silvestres - monitoramento/main.c
@@ -11,12 +11,12 @@ int main()
 {
   FILE *fpe;
   char opcao;
-  char linha[50],*result,*mudar; 
+  char linha[50],copia[50],*result,*mudar,*t,*k; 
   char procura[50];
   INDIVIDUOS inserir; 
   char at_indivi[5],at_especie[5];	      
 
-
+  long size;
  
   printf(OPT_IND);
   scanf("%c",&opcao);
@@ -32,7 +32,7 @@ int main()
     }
     else{
       ler_individuos(fpe);   
-	fclose(fpe);
+      fclose(fpe);
     }
     break;
   case 'I':
@@ -55,16 +55,25 @@ int main()
     if(toupper(opcao)=='A'){col=0;}
     else if(toupper(opcao)=='B'){col=1;}
 
- printf("Qual individuo deve ter seus dados ataulizados: \n");  
+    printf("Qual individuo deve ter seus dados ataulizados: \n");  
     scanf("%s",procura);
     while(fgetc(fpe)!=EOF){
+      size=ftell (fpe);
+
       fgets(linha,50,fpe);  
+      strcpy(copia,linha);
+
+	    
       result = strtok( linha, "#" );
+    
       contador=0;  
       while( result != NULL ) {
 	if(contador ==col){
 	  if(strcmp(result,procura)==0){
+
 	    printf("O que deseja atualizar?\n");
+
+
 	    printf("(A)-Sexo \n");
 	    printf("(B)-Numero de identificaçao do invidividuo\n");
 	    printf("(C)-Numero de identificaçao da especie\n");
@@ -75,45 +84,50 @@ int main()
 	    printf("encontrado %s\n",result);
 	    scanf(" %c",&opcao); 
 	    
-	    mudar = strtok( linha, "#" );
 	    
 	    switch(toupper(opcao))
 	      {
 		
-		
-	    case 'A':
-	      printf(IND_SEX);
-	      scanf(" %c",&(inserir.sexo));
+	      case 'A':
+		printf(IND_SEX);
+		scanf(" %c",&(inserir.sexo));
+		contador=0;
 	      
-	      //	      strcpy(at_especie,mudar);
-	      // mudar = strtok( NULL,"#" );
-	      //   inserir.num_individuo=*mudar;
-	      //    strcpy(at_indivi,mudar);
-	   
-	      fprintf(fpe,"#%s#%s#%c\n",mudar, strtok( NULL,"#" ),inserir.sexo);
-	      break;
-	    case 'G':
-	      printf(IND_ESPC);
-	      scanf(" %d",&(inserir.num_especie));
-	      printf(IND_INDIV);
-	      scanf(" %d",&(inserir.num_individuo));
-	      printf(IND_SEX);
-	      scanf(" %c",&(inserir.sexo));
-	      fprintf(fpe,"#%d#%d#%c\n",inserir.num_especie,inserir.num_individuo,inserir.sexo);
-	      break;
+		mudar = strtok(copia, "#" );
+	 	 
+		while  (mudar != NULL)
+		  {
+		    if(contador==1){ k=mudar;contador++;}
+		    if(contador==0){ t=mudar;contador++;}
+		    mudar=strtok(NULL,"#");
+	
+		  }
+		fseek (fpe, size-1, 0);
+
+		fprintf(fpe,"#%s#%s#%c\n",t,k,inserir.sexo);
+		break;
+	      case 'G':
+		printf(IND_ESPC);
+		scanf(" %d",&(inserir.num_especie));
+		printf(IND_INDIV);
+		scanf(" %d",&(inserir.num_individuo));
+		printf(IND_SEX);
+		scanf(" %c",&(inserir.sexo));
+		fprintf(fpe,"#%d#%d#%c\n",inserir.num_especie,inserir.num_individuo,inserir.sexo);
+		break;
 	    
 	    
-	    }  
+	      }  
 	  }
 	}
-	  result = strtok( NULL,"#" );
-	  contador++;
+	result = strtok( NULL,"#" );
+	contador++;
       }
     
     }
     fclose(fpe);
       
- break;
+    break;
     
   case 'R':
     break;
