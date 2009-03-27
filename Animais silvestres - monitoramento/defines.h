e87464c43d08d5c8c28963c91ce1403f8d7e4feb
tiagochst@gmail.com <tiagochst@gmail.com@ab741a24-0e9a-11de-99c7-9d618077940f>
2009-03-27 02:51:23 +0000

    nova implementação para especies.txt problemas: 1- ler espaços 2- leitura do arquivo - tem tamanho não variavel,muito grande,mas mais fácil para atualizar opcao na atualiazacao ler e por no fim da linha mudanca e remover antigo. remover não implementei ainda nem atualizar

--- Animais silvestres - monitoramento/defines.h
+++ Animais silvestres - monitoramento/defines.h
@@ -1,12 +1,24 @@
+/* pedidos de entrada especies.txt*/
+#define ESP_ESPC "\nPor favor,entre com o indentificador unico da especie:  "
+#define ESP_DATA "\nPor favor,entre com a data da especie:  "
+#define ESP_FOTO "\nPor favor,entre com o caminho da foto:  "
+#define ESP_NCIE "\nPor favor,entre com o nome cientifico:  "
+#define ESP_NPOP "\nPor favor,entre com o nome popular :  "
+#define ESP_DESC "\nPor favor,entre com a descrição da espécie:  "
+
+
+/* pedidos de entrada individuos.txt*/
 #define IND_ESPC "\nPor favor,entre com o indentificador unico da especie:  "
 #define IND_INDIV "\nPor favor,entre com o indentificador unico do individuo:  "
 #define IND_SEX "\nPor favor,entre com o sexo:  "
 
+/*erro na manipulação do arquivo */
 #define ERRO_ARQ "Arquivo de entrada nao existe ou sem premissao de leitura.\n"
 
-#define OPT_IND "Opcoes de tarefas relacionada aos dados de individuos:\n -Ver todos os individuos listados (L)\n -Inserir novo individuo(I)\n -Inserir atualizar dados de individuo(A)\n -Voltar Menu(V)\n -Sair(S)\n Por favor faça sua escolha: "
-
-
+/* Menus de escolha do usuario*/
+#define OPT_ARQ "O que deseja manipular? :\n -(A)Especies\n -(B)Individuos\n -(E)Sair\n"
+#define OPT_IND "Opcoes de tarefas relacionada aos dados de individuos:\n -(L)Ver todos os individuos listados\n -(I)Inserir novo individuo\n -(A)Atualizar dados de individuo\n -(V)Voltar Menu\n -(S)Sair\n Por favor faça sua escolha: "
+#define OPT_ESP "Opcoes de tarefas relacionada aos dados de especies:\n -(L)Ver todos os especies listados e informacoes\n -(I)Inserir nova especie\n -(A)Atualizar dados de individuo\n -(V)Voltar Menu\n -(S)Sair\n Por favor faça sua escolha: "
 
 
 
@@ -17,4 +29,11 @@ typedef struct {
 
 }INDIVIDUOS;
 
+
+typedef struct {
+
+  int num_especie,data;
+  char nome_cientifico[30],nome_popular[30],descricao[500],caminho_foto[255];
+
+}ESPECIES;
 
